2c88d8b94e9941dca7a5cb376d86763375af9ae6
WojciechGalecki <wojciech.galecki@wp.pl>
2020-06-04 19:53:03 +0200

    created user and account microservices as a eureka clients

---
+++ user/src/main/java/wg/account/controllers/UserController.java
@@ -0,0 +1,15 @@
+package wg.account.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/demo")
+    public String demo() {
+        return "ok";
+    }
+}
