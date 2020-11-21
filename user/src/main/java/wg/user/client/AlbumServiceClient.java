e651c706a40a2029c655ffea5aa9fc96c95b5a7b
WojciechGalecki <wojciech.galecki@wp.pl>
2020-11-21 20:06:47 +0100

    added fallback factory for logging errors

--- user/src/main/java/wg/user/client/AlbumServiceClient.java
+++ user/src/main/java/wg/user/client/AlbumServiceClient.java
@@ -1,5 +1,7 @@
 package wg.user.client;
 
+import feign.hystrix.FallbackFactory;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,7 +11,7 @@ import wg.user.model.AlbumResponse;
 import java.util.Collections;
 import java.util.List;
 
-@FeignClient(name = "album-service", fallback = AlbumFallBack.class)
+@FeignClient(name = "album-service", fallbackFactory = AlbumFallbackFactory.class)
 public interface AlbumServiceClient {
 
     @GetMapping("/users/{id}/albums")
@@ -17,10 +19,24 @@ public interface AlbumServiceClient {
 }
 
 @Component
-class AlbumFallBack implements AlbumServiceClient {
+class AlbumFallbackFactory implements FallbackFactory<AlbumServiceClient> {
+    @Override
+    public AlbumServiceClient create(Throwable cause) {
+        return new AlbumFallback(cause);
+    }
+}
+
+@Slf4j
+class AlbumFallback implements AlbumServiceClient {
+    private final Throwable cause;
+
+    public AlbumFallback(Throwable cause) {
+        this.cause = cause;
+    }
 
     @Override
     public List<AlbumResponse> getAlbums(String id) {
+        log.error("An error occurred during calling the Album Service: {}", cause.getLocalizedMessage());
         return Collections.emptyList();
     }
 }
