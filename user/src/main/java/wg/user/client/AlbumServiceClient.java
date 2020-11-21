8f3ecc996d385e8ed09fbac3cccb52e498961d6a
WojciechGalecki <wojciech.galecki@wp.pl>
2020-11-21 19:40:40 +0100

    configured circuit breaker with feign client fallback option

--- user/src/main/java/wg/user/client/AlbumServiceClient.java
+++ user/src/main/java/wg/user/client/AlbumServiceClient.java
@@ -1,15 +1,26 @@
 package wg.user.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import wg.user.model.AlbumResponse;
 
+import java.util.Collections;
 import java.util.List;
 
-@FeignClient(name = "album-service")
+@FeignClient(name = "album-service", fallback = AlbumFallBack.class)
 public interface AlbumServiceClient {
 
-    @GetMapping("/users/{id}/albums1")
+    @GetMapping("/users/{id}/albums")
     List<AlbumResponse> getAlbums(@PathVariable String id);
+}
+
+@Component
+class AlbumFallBack implements AlbumServiceClient {
+
+    @Override
+    public List<AlbumResponse> getAlbums(String id) {
+        return Collections.emptyList();
+    }
 }
